0b32698faaae9b82577f21394355f34f67f9ad3c
Alex Felipe <alexfelipevieira@gmail.com>
2021-04-26 15:37:30 -0300

    1.5

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -1,6 +1,14 @@
 package br.com.alura.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Bem vindo(a) ao Orgs!", Toast.LENGTH_SHORT).show()
+    }
+
 }
