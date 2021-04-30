61af4cdccae489dd241f7a2f42e7bc1e81e7ff66
Alex Felipe <alexfelipevieira@gmail.com>
2021-04-30 10:23:10 -0300

    4.2

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -11,12 +11,12 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val nome = findViewById<TextView>(R.id.nome)
-        nome.text = "Cesta de frutas"
-        val descricao = findViewById<TextView>(R.id.descricao)
-        descricao.text = "Laranja, manga e maçã"
-        val valor = findViewById<TextView>(R.id.valor)
-        valor.text = "19.99"
+//        val nome = findViewById<TextView>(R.id.nome)
+//        nome.text = "Cesta de frutas"
+//        val descricao = findViewById<TextView>(R.id.descricao)
+//        descricao.text = "Laranja, manga e maçã"
+//        val valor = findViewById<TextView>(R.id.valor)
+//        valor.text = "19.99"
     }
 
 }
