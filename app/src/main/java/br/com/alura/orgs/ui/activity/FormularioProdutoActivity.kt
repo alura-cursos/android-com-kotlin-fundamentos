fb18aa35102a2eb693d7ac5dabcf0a04a2fd597c
Alex Felipe <alexfelipevieira@gmail.com>
2021-05-05 14:51:44 -0300

    5.1

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,14 @@
+package br.com.alura.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.alura.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+    }
+
+}
