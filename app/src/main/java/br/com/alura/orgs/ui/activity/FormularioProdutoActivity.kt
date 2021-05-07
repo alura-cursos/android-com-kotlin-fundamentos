7f5a359c6ea299539fe0e90033220cca41c98867
Alex Felipe <alexfelipevieira@gmail.com>
2021-05-07 14:48:02 -0300

    6.3

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,9 +2,24 @@ package br.com.alura.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import br.com.alura.orgs.R
 
 class FormularioProdutoActivity :
     AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            Log.i("FormularioProduto", "onCreate: $nome")
+        }
+    }
 
 }
